3cd7cc1a1beded8cd44a60fb764e77407384445d
hatanaca <thiagohatanaka18@gmail.com>
2025-11-21 14:14:10 -0300

    estudando objetos

--- Untitled-1.js
+++ Untitled-1.js
@@ -62,3 +62,53 @@ if (!(tabelaVdd[1] && tabelaVdd2[0])) console.log("Operador E com negação");
 if ((tabelaVdd[0] || tabelaVdd[3]) && (tabelaVdd[0] != tabelaVdd2[3])) console.log("XOR com operadores básicos");
 
 
+function regularFunction() {
+    const expressFunction = function() {
+        return this;
+    }
+    return console.log(expressFunction());
+    
+}  
+regularFunction();
+
+
+const arrowFunction = () => {
+    return "arrowFunction with more letters";
+}
+
+const shortArrowFunction = x => x * x;
+
+console.log(shortArrowFunction(5));
+
+
+
+class Digimon {
+    constructor(name, level, element) {
+        this.name = name;
+        this.level = level;
+        this.element = element;
+    }
+
+    elemento(element) {
+        this.element = "SEILA"
+    }
+
+    Hp = 100;
+
+    Atacke(target) {
+        if ((!target instanceof Digimon)) {
+            throw new Error("Target must be a Digimon");
+        }
+        target.Hp = target.Hp - 10;
+        console.log(`${this.name} attacked ${target.name}, ${target.name} has ${target.Hp} Hp left. `)
+        
+    }
+}
+
+const Agumon = new Digimon("Agumon", "Rookie", "Fire");
+
+const Gabumon = new Digimon("Gabumon", "Rookie");
+
+Agumon.Atacke(Gabumon);
+Gabumon.Atacke(Agumon);
+
